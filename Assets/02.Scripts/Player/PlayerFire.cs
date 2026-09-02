8755c2ab52666273049708d0d19d7954953f8c85
김민수 <kms99400@gmail.com>
2026-09-02 14:34:56 +0900

    feat: 총알 양쪽 발사 구현
    이제 한쪽이 아닌 양쪽으로 총알이 발사

--- Assets/02.Scripts/Player/PlayerFire.cs
+++ Assets/02.Scripts/Player/PlayerFire.cs
@@ -7,17 +7,25 @@ public class PlayerFire : MonoBehaviour
     // - 총알 프리팹
     public GameObject BulletPrefab;
     // - 생성 위치(총구)
-    public Transform FirePoint;
+    public Transform RightFirePoint;
+    public Transform LeftFirePoint;
     
     private void Update()
-    {   
+    {
+        BulletFire();
+    }
+
+    private void BulletFire()
+    {
         // 1. 스페이스바를 누르면
         if (Input.GetKeyDown(KeyCode.Space))
         {
             // 2. 총알 프리팹을 생성한다.
             // Instantiate는 프리팹을 복사해서 (MonoBehaviour를 상속받는)게임 오브젝트를 만들고 씬에 넣어주는 기능
-            GameObject bullet = Instantiate(BulletPrefab);
-            bullet.transform.position = FirePoint.position;    // 생성한 총알의 위치를 나(플레이어)의 위치로
+            GameObject rightBullet = Instantiate(BulletPrefab);
+            GameObject leftBullet = Instantiate(BulletPrefab);
+            rightBullet.transform.position = RightFirePoint.position;    // 생성한 총알의 위치를 나(플레이어)의 위치로
+            leftBullet.transform.position =  LeftFirePoint.position;
         }
     }
 }
