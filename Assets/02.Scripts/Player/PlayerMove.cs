2e7f573c57ef86ec1b9158d3e890cf6d06772fae
김민수 <kms99400@gmail.com>
2026-09-02 12:25:04 +0900

    feat: 플레이어 이동 영역 제한
    플레이어가 화면 하단 부분의 특정 영역을 벗어날 수 없도록 처리

--- Assets/02.Scripts/Player/PlayerMove.cs
+++ Assets/02.Scripts/Player/PlayerMove.cs
@@ -9,6 +9,11 @@ public class PlayerMove : MonoBehaviour
     
     // 필요 필드:
     public float Speed;
+
+    public float MinX = -2.4f;
+    public float MaxX = 2.4f;
+    public float MinY = -4.7f;
+    public float MaxY = 0f;
     
     // 매 프레임마다 실행된다.
     // 초당 프레임 실행 횟수 : 별다른 설정이 없을 경우 가능한 많이 진행
@@ -19,30 +24,19 @@ public class PlayerMove : MonoBehaviour
         float v = Input.GetAxisRaw("Vertical");    // 키보드 위/아래 입력 상태에 따라 -1f ~ 0 ~ 1f
         
         Debug.Log($"h: {h}, v: {v}");
-        
-        // if (Input.GetKey(KeyCode.LeftArrow))
-        // {
-        //     Debug.Log("왼쪽 방향키를 누르는 중");
-        //     
+ 
         // 2. 키보드 입력에 따라 방향을 구한다.
         Vector2 direction = new Vector2(h, v); // 왼쪽 방향
         
-        //     // 게임에는 벡터라는 타입이 있다. 벡터는(크기와 방향을 의미한다)
-        //     Vector2 direction = new Vector2(-1, 0); // 왼쪽 방향
-        //     // Vector2 direction = Vector2.left;
-        //
-        //     // 3. 방향과 속력에 따라 이동한다.
-        //     // 속도 = 방향 * 속력                         
-        //     // 매직 넘버란 : 보는 사람에 따라 의미가 달라질 수 있는 것
-        //     // 헷갈리는 숫자. 여기서는 0.05f 가 매직넘버에 해당
-        //     transform.Translate(direction * Speed * Time.deltaTime);
-        //     // deltaTime: 이전 프레임으로부터 지금 프레임까지 시간이 얼마나 지났는지 m/s로 반환 (1000분의 1초)
-        // }
-        
         // 3. 방향과 속력에 따라 이동한다.
         Vector2 normalizedSpeed = (direction * Speed).normalized; // 벡터의 길이를 1로 만들어주는 것 (즉, 방향만 유지한다.)
-        
         // 새로운 위치 = 현재 위치 + (방향 * 속력 * 시간)
-        transform.position += (Vector3)(direction * normalizedSpeed * Time.deltaTime);
+        transform.Translate(normalizedSpeed * Time.deltaTime);
+        
+        // 4. 플레이어 이동 영역을 제한한다.
+        float posX = Mathf.Clamp(transform.position.x, MinX, MaxX);
+        float posY = Mathf.Clamp(transform.position.y, MinY, MaxY);
+        
+        transform.position = new Vector2(posX, posY);
     }
 }
