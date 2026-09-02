73975716058169d76d5b3cad5aab74782ccbcb04
김민수 <kms99400@gmail.com>
2026-09-02 15:34:22 +0900

    feat: 공격 모드 토글 추가
    1번 키를 누르면 자동 공격 모드와 수동 공격 모드를 사용 가능

--- Assets/02.Scripts/Player/PlayerFire.cs
+++ Assets/02.Scripts/Player/PlayerFire.cs
@@ -13,29 +13,52 @@ public class PlayerFire : MonoBehaviour
     public float FireCooldown = 0.5f;
     private float LastFireCooldown = 0f;
     
+    private bool isNumber1KeyPressed = false;
+    
     private void Update()
     {
-        BulletFire();
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            isNumber1KeyPressed = !isNumber1KeyPressed;
+        }
+
+        if (!isNumber1KeyPressed)
+        {
+            SpaceBulletFire();
+        }
+        else if (isNumber1KeyPressed)
+        {
+            AutoBulletFire();
+        }
     }
 
     private void BulletFire()
     {
-        // 1. 스페이스바를 누르면
+        // 2. 총알 프리팹을 생성한다.
+        // Instantiate는 프리팹을 복사해서 (MonoBehaviour를 상속받는)게임 오브젝트를 만들고 씬에 넣어주는 기능
+
+        if (Time.time >= LastFireCooldown + FireCooldown)
+        {
+            LastFireCooldown = Time.time;
+                
+            GameObject rightBullet = Instantiate(BulletPrefab);
+            GameObject leftBullet = Instantiate(BulletPrefab);
+            
+            rightBullet.transform.position = RightFirePoint.position;    // 생성한 총알의 위치를 나(플레이어)의 위치로
+            leftBullet.transform.position =  LeftFirePoint.position;
+        }
+    }
+
+    private void SpaceBulletFire()
+    {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            // 2. 총알 프리팹을 생성한다.
-            // Instantiate는 프리팹을 복사해서 (MonoBehaviour를 상속받는)게임 오브젝트를 만들고 씬에 넣어주는 기능
-
-            if (Time.time >= LastFireCooldown + FireCooldown)
-            {
-                LastFireCooldown = Time.time;
-                
-                GameObject rightBullet = Instantiate(BulletPrefab);
-                GameObject leftBullet = Instantiate(BulletPrefab);
-            
-                rightBullet.transform.position = RightFirePoint.position;    // 생성한 총알의 위치를 나(플레이어)의 위치로
-                leftBullet.transform.position =  LeftFirePoint.position;
-            }
+            BulletFire();
         }
+    }
+    
+    private void AutoBulletFire()
+    {
+        BulletFire();
     }
 }
