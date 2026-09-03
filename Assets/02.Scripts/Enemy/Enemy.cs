a3e74a688b0ba4ac6944a3fa3491b674cab6ab4a
김민수 <kms99400@gmail.com>
2026-09-03 11:59:05 +0900

    feat: 적 오브젝트 생성
    게임 시작시 적 3개가 아래로 내려온다

---
+++ Assets/02.Scripts/Enemy/Enemy.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float Speed;
+
+    public GameObject EnemyPrefabs;
+
+    public Transform LeftSpawnPoint;
+    public Transform CenterSpawnPoint;
+    public Transform RightSpawnPoint;
+
+    private void Update()
+    {
+        Vector2 direction = Vector2.down;
+
+        transform.Translate(direction * Speed * Time.deltaTime);
+    }
+
+    // private void EnemySpawn()
+    // {
+    //     GameObject LeftEnemy = Instantiate(EnemyPrefabs);
+    //     GameObject CenterEnemy = Instantiate(EnemyPrefabs);
+    //     GameObject RightEnemy = Instantiate(EnemyPrefabs);
+    //
+    //     LeftEnemy.transform.position = LeftSpawnPoint.position;
+    //     CenterEnemy.transform.position = CenterSpawnPoint.position;
+    //     RightEnemy.transform.position = RightSpawnPoint.position;
+    // }
+}
