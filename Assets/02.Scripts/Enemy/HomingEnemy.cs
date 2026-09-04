dd27348474796dd9bad248a51a8070911590fee0
김민수 <kms99400@gmail.com>
2026-09-04 10:42:37 +0900

    refactor: 캐싱 기법 재적용 및 원복

--- Assets/02.Scripts/Enemy/HomingEnemy.cs
+++ Assets/02.Scripts/Enemy/HomingEnemy.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 
 public class HomingEnemy : Enemy
 {
+    // 캐싱 : 자주 쓸법한 데이터(객체)를 가까운 곳에 저장해두고 쓰는 것
     private GameObject _player;
 
     private void Start()
     {
+        _player = GameObject.FindWithTag("Player");
         if (_player == null)
         {
             Debug.Log("플레이어 태그를 가진 플레이어를 찾지 못했습니다.");
@@ -14,8 +16,6 @@ public class HomingEnemy : Enemy
 
     protected override void Move()
     {
-        _player = GameObject.FindWithTag("Player");
-
         // 1. 방향을 구한다. (상대방 위치 - 내 위치)
         Vector2 direction = _player.transform.position - transform.position;
         direction.Normalize();
