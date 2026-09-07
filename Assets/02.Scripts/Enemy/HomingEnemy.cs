5722af42d9a5ef6be20eb5b383623713cbba38a5
means-kim <kms99400@gmail.com>
2026-09-07 21:07:40 +0900

    refactor: Enemy 끼리 충돌하지 않게 레이어 조정

--- Assets/02.Scripts/Enemy/HomingEnemy.cs
+++ Assets/02.Scripts/Enemy/HomingEnemy.cs
@@ -9,6 +9,11 @@ public class HomingEnemy : Enemy
     private void Start()
     {
         _player = GameObject.FindWithTag("Player");
+        if (_player == null)
+        {
+            Debug.Log("플레이어 태그를 가진 플레이어를 찾지 못했습니다.");
+            return;
+        }
     }
 
     protected override void Move()
