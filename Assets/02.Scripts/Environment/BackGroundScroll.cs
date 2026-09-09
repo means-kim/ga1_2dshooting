df9b087d24681a015bf72fd4c1a02e114a8f4354
김민수 <kms99400@gmail.com>
2026-09-09 14:10:18 +0900

    feat: Parallax Map으로 원근감 구현

--- Assets/02.Scripts/Environment/BackGroundScroll.cs
+++ Assets/02.Scripts/Environment/BackGroundScroll.cs
@@ -15,6 +15,8 @@ public class BackGroundScroll : MonoBehaviour
     private void Update()
     {
         _offsetY += _scrollSpeed * Time.deltaTime;
+
+        // ToDo: Material 프로퍼티 블록을 활용한 최적화
         _material.mainTextureOffset = new Vector2(0, _offsetY);
     }
 }
