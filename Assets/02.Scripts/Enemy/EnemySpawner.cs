f92472847a62b78b780cf4c252b44cd2ac370b00
김민수 <kms99400@gmail.com>
2026-09-11 10:20:36 +0900

    feat: 적 스폰 가중치 기반 랜덤 선택
    - EnemySpawnData 데이터 클래스 생성

--- Assets/02.Scripts/Enemy/EnemySpawner.cs
+++ Assets/02.Scripts/Enemy/EnemySpawner.cs
@@ -18,8 +18,10 @@ public class EnemySpawner : MonoBehaviour
     private float _timer;
     private float _spawnProbability;
 
+    [SerializeField] private EnemySpawnData[] _spawnDatas;
+
     // - 생설할 프리팹
-    [SerializeField] private Enemy[] _enemyPrefab;
+    // [SerializeField] private Enemy[] _enemyPrefab;
     // [SerializeField] private Enemy _aimingEnemyPrefab;
     // [SerializeField] private Enemy _homingEnemyPrefab;
 
@@ -45,20 +47,47 @@ public class EnemySpawner : MonoBehaviour
         // Todo: Scriptable Object를 사용해서 리팩토링
         // 이유 1: 배열을 사용했지만 각 아이템이 어떤 프리팹인지 알 수가 없다.
         // 이유 2: 각 Enemy 스폰 확률을 하드코딩해서 유지보수가 어렵고 가독성 저하
-        if (_spawnProbability < 2f)
+
+        // 가중치 랜덤 선택
+        // 각 항목에 가중치를 부여하고, 가중치가 클수록 높은 확률로 선택되도록 하는 방식
+
+        // 1. 추첨할 수 있는 모든 가중치를 더한다.
+        int totalWeight = 0;
+        foreach (EnemySpawnData data in _spawnDatas)
         {
-            Enemy enemy = Instantiate(_enemyPrefab[(int)EnemyType.Homing]);
-            enemy.transform.position = transform.position;
+            totalWeight += data.Weight;
         }
-        else if (_spawnProbability < 5f)
+
+        // 2. 전체 가중치 범위에서 랜덤한 정수를 뽑는다.
+        int randomWeight = Random.Range(0, totalWeight);
+
+        // 3. 가중치를 누적하면서 선택된 구간을 찾는다.
+        int cumulativeWeight = 0;
+        foreach (EnemySpawnData data in _spawnDatas)
         {
-            Enemy enemy = Instantiate(_enemyPrefab[(int)EnemyType.Aiming]);
-            enemy.transform.position = transform.position;
+            cumulativeWeight += data.Weight; // 누적
+            if (randomWeight < cumulativeWeight) // 구간
+            {
+                GameObject enemy = Instantiate(data.EnemyPrefab);
+                enemy.transform.position = transform.position;
+                break;
+            }
         }
-        else
-        {
-            Enemy enemy = Instantiate(_enemyPrefab[(int)EnemyType.Normal]);
-            enemy.transform.position = transform.position;
-        }
+
+        // if (_spawnProbability < 2f)
+        // {
+        //     Enemy enemy = Instantiate(_enemyPrefab[(int)EnemyType.Homing]);
+        //     enemy.transform.position = transform.position;
+        // }
+        // else if (_spawnProbability < 5f)
+        // {
+        //     Enemy enemy = Instantiate(_enemyPrefab[(int)EnemyType.Aiming]);
+        //     enemy.transform.position = transform.position;
+        // }
+        // else
+        // {
+        //     Enemy enemy = Instantiate(_enemyPrefab[(int)EnemyType.Normal]);
+        //     enemy.transform.position = transform.position;
+        // }
     }
 }
